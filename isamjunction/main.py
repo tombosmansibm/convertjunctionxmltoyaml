d0ca0e4256dea411af2399dfc22ff66707627825
tbosmans <tom.bosmans@be.ibm.com>
2021-05-25 09:49:33 +0200

    add command line option

--- isamjunction/main.py
+++ isamjunction/main.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
+"""
+Usage:  main.py
+        main.py [--junctiondir=JUNCTIONDIR]
+
+Options:
+  --junctiondir=DIR         directory that contains the junctions
+  -h --help     Show this screen.
+
+"""
 from lib import f_processJunction as f_processJunction
 import os
+from docopt import docopt
 import tkinter
 from tkinter import filedialog
 
 root = tkinter.Tk()
 root.withdraw()
 
-junctiondir = filedialog.askdirectory(mustexist=True)
+from docopt import docopt
 
-if junctiondir is not None and len(junctiondir) > 0:
-    for junctionfilepath in os.listdir(junctiondir):
-        print("\n\nOpening file " + junctiondir + "/" + junctionfilepath)
-        f_processJunction(junctiondir+"/"+junctionfilepath)
+def main():
+    junctiondir = ''
+    args = docopt(__doc__)
+    print(args)
+    if args['--junctiondir']:
+        #ok
+        junctiondir = args['--junctiondir']
+        print(junctiondir)
+    if junctiondir == '':
+        junctiondir = filedialog.askdirectory(mustexist=True)
 
+    if junctiondir is not None and len(junctiondir) > 0:
+        for junctionfilepath in os.listdir(junctiondir):
+            print("\n\nOpening file " + junctiondir + "/" + junctionfilepath)
+            f_processJunction(junctiondir+"/"+junctionfilepath)
 
-
-
+if __name__=='__main__':
+   main()
